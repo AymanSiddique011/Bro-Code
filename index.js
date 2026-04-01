52ac65adf13d8c6648cd43214e29be2286c9700b
Mohammad Ayman Siddique <mdayman302010@gmail.com>
2026-04-01 17:11:11 +0600

    Update

--- index.js
+++ index.js
@@ -3,4 +3,25 @@ let person={
     age: 21,
     salary: null,
 }
-console.log(person)
+//console.log(person)
+//console.log("I like pizza!");
+//window.alert("This is an alert!");
+
+//document.getElementById("myH1").textContent= "Hello";
+//document.getElementById("myP").textContent= "I like pizza";
+
+
+// Variable= A container that stores a value.
+//           Behaves as if it were the value it contains.
+
+// 1. declaration let x;
+// 2. assignment x= 100;
+
+/* let x;
+x= 100;
+console.log(x); */
+
+let age = 21;
+let price = 20.69;
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
