2d1945a01cdd8c867fbe51641447162ac62695dd
Mohammad Ayman Siddique <mdayman302010@gmail.com>
2026-04-02 22:51:05 +0600

    Practice

---
+++ arithmetic.js
@@ -0,0 +1,35 @@
+// Arithmetic operators = operands (Values, Variables, etc.)
+//                        oeprators (+ - * /)
+//                        ex. 11 = x+5;
+
+let students = 31;
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2;
+//students = students % 3;
+//let extraStudents = students % 3;
+
+//students += 30;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students **=2;
+//students %=2;
+//students ++;
+//students --;
+
+//console.log(students);
+
+
+/* Operator precedence
+   1. Paremthesis ()
+   2. Exponents
+   3. Multiplication & Division & Modulus
+   4. Addition & Subtraction
+*/
+
+let result = 12 % 5 + 7 / 2;
+console.log(result);
+
--- index.js
+++ index.js
@@ -21,7 +21,35 @@ let person={
 x= 100;
 console.log(x); */
 
-let age = 21;
-let price = 20.69;
-console.log(`You are ${age} years old`);
-console.log(`The price is $${price}`);
+//let age = 21;
+//let price = 20.69;
+//let gpa = 5.00;
+//console.log(`You are ${age} years old`);
+//console.log(`The price is $${price}`);
+//console.log(`My GPA is ${gpa}`); 
+
+//let firstName = "Ayman Siddique";
+//let favouriteFood = "Pizza";
+//let favouritemovieName = "Intersteller";
+//let email = "call.me.mindflayer011@gmail.com"
+//console.log(firstName);
+//console.log(typeof firstName);
+//console.log(`My name is ${firstName}`);
+//console.log(`My favourite food is ${favouriteFood}`);
+//console.log(`My favourite movie is ${favouritemovieName}`);
+//console.log(`My email is ${email}`);
+
+//let online = true;
+//let forSale = false;
+//let isStudent = true;
+//console.log(typeof online)
+//console.log(`Bro is online: ${online}`);
+//console.log(`Is this car for sale? : ${forSale}`);
+//console.log(`Enrolled ${isStudent}`);
+
+//let fullName= "Walter White"
+//let age= 48;
+//let isTeacher= true;
+//document.getElementById("p1").textContent = `His name is ${fullName}`;
+//document.getElementById("p2").textContent = `His age is ${age}`;
+//document.getElementById("p3").textContent = `He is a chemistry teacher: ${isTeacher}`;
