4d7d390f85f570848490e136c3905f6890814137
Mohammad Ayman Siddique <mdayman302010@gmail.com>
2026-04-05 10:17:18 +0600

    UPDATE

--- type conversion.js
+++ type conversion.js
@@ -1,9 +1,35 @@
 // type conversion = change the datatype of a value to another data type
 //                   (Strings, Numbers, Booleans)
 
-let age = window.prompt("How old are you?");
-21
-age = Number(age);21
-
+/* let age = window.prompt("How old are you?");
+age = Number(age);
 age+=1;
-console.log(age ,typeof age);
+console.log(age ,typeof age); */
+
+// Another Example
+
+let x = "Pizza";
+let y = "Burger";
+let z = "Pasta";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+// console.log (x, typeof x);
+// console.log (y, typeof y);
+// console.log (z, typeof z);
+
+
+let a;
+let b;
+let c;
+
+// a = Number(a);
+// b = String(b);
+// c = Boolean(c);
+
+console.log (a, typeof a);
+console.log (b, typeof b);
+console.log (c, typeof c);
+
